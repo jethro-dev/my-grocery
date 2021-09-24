4d59b9e067b170aaafd46788b1356245d74bca39
Jethro Au <supergalong@gmail.com>
2021-09-24 22:52:16 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # my-grocery
+
+## Welcome! 👋
+
+This is a grocery list app made with ReactJS.
+
+## Build with ⚙️
+
+This project is built with HTML, CSS and ReactJS.
+The app used Local Storage.
+
+## Screenshots 📷
+
+![](img/screenshot1.png)
+
+## Link 🔗
+
+[Click Me!](https://my-menu-component.netlify.app/)
---
+++ build/precache-manifest.32f19506a1070896a81877b31fbd019c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a663e86cf1cdca8c7670969f75f500b1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e065542f01ea73c57bb7",
+    "url": "/static/css/main.a903c182.chunk.css"
+  },
+  {
+    "revision": "f86b35d4e5ed4a99a4db",
+    "url": "/static/js/2.22b6d6b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.22b6d6b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e065542f01ea73c57bb7",
+    "url": "/static/js/main.3507838f.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
